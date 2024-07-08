da6c99ef232a438cf15b57e126b9a6aad1651279
Ali Ahasan <ZermaSines@protonmail.com>
2024-07-08 21:53:11 +0600

    Same as before - Only for Assignment-1

--- Assignment_1_Ali_Ahasan/js/custom.js
+++ Assignment_1_Ali_Ahasan/js/custom.js
@@ -1,12 +1,3 @@
-// to get current year
-// function getYear() {
-//     var currentDate = new Date();
-//     var currentYear = currentDate.getFullYear();
-//     document.querySelector("#displayYear").innerHTML = currentYear;
-// }
-
-// getYear();
-
 // nice select
 $(document).ready(function () {
     $('select').niceSelect();
@@ -20,27 +11,10 @@ $(function () {
     }).datepicker('update', new Date());
 });
 
-// owl carousel slider js
-$('.team_carousel').owlCarousel({
-    loop: true,
-    margin: 15,
-    dots: true,
-    autoplay: true,
-    navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1,
-            margin: 0
-        },
-        576: {
-            items: 2,
-        },
-        992: {
-            items: 3
-        }
-    }
-})
+document.addEventListener('DOMContentLoaded', function () {
+    var myCarousel = document.querySelector('#customCarousel1');
+    var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 1000,
+      wrap: true
+    });
+});
