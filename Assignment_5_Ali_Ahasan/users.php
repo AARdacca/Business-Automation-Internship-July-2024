a7a0612fd63f2e23b3cea055144cc0f2b16af8b8
Ali Ahasan <ZermaSines@protonmail.com>
2024-08-20 07:50:05 +0600

    Updated Assignment-5 - Updated the users.php

--- Assignment_5_Ali_Ahasan/users.php
+++ Assignment_5_Ali_Ahasan/users.php
@@ -11,41 +11,18 @@ function saveDataToCookie() {
     setcookie('user_data', serialize($_SESSION['users']), time() + (86400 * 30), "/"); // 30 days expiration
 }
 
-// Function to clear cookie data
-function clearCookie() {
-    setcookie('user_data', '', time() - 3600, "/"); // Expire the cookie
-}
-
-// Delete a specific user from session
-if (isset($_GET['delete_id_session'])) {
-    array_splice($_SESSION['users'], $_GET['delete_id_session'], 1);
+// Delete a specific user
+if (isset($_GET['delete_id'])) {
+    array_splice($_SESSION['users'], $_GET['delete_id'], 1);
     saveDataToCookie();
     header("Location: users.php");
     exit();
 }
 
-// Delete a specific user from cookie
-if (isset($_GET['delete_id_cookie'])) {
-    $users = unserialize($_COOKIE['user_data']);
-    array_splice($users, $_GET['delete_id_cookie'], 1);
-    $_SESSION['users'] = $users; // Update session to reflect cookie change
-    saveDataToCookie();
-    header("Location: users.php");
-    exit();
-}
-
-// Delete all users from session
-if (isset($_GET['delete_all_session'])) {
+// Delete all users
+if (isset($_GET['delete_all'])) {
     $_SESSION['users'] = [];
-    saveDataToCookie(); // Update cookie to reflect session change
-    header("Location: users.php");
-    exit();
-}
-
-// Delete all users from cookie
-if (isset($_GET['delete_all_cookie'])) {
-    clearCookie();
-    $_SESSION['users'] = []; // Clear session as well
+    setcookie('user_data', '', time() - 3600, "/"); // Expire the cookie
     header("Location: users.php");
     exit();
 }
@@ -54,17 +31,10 @@ echo "<h1>Registered Users</h1>";
 echo "<ul>";
 foreach ($_SESSION['users'] as $index => $user) {
     echo "<li>" . htmlspecialchars($user['username']) . " - " . htmlspecialchars($user['email']) . " - " . htmlspecialchars($user['address']) .
-    " <a href='users.php?delete_id_session=$index'>Delete from Session</a> | <a href='users.php?delete_id_cookie=$index'>Delete from Cookie</a></li>";
+    " <a href='users.php?delete_id=$index'>Delete</a></li>";
 }
 echo "</ul>";
-
-if (!empty($_SESSION['users'])) {
-    echo "<a href='users.php?delete_all_session=true'>Delete All Users from Session</a><br>";
-}
-
-if (isset($_COOKIE['user_data'])) {
-    echo "<a href='users.php?delete_all_cookie=true'>Delete All Users from Cookie</a>";
-}
+echo "<a href='users.php?delete_all=true'>Delete All Users</a>";
 ?>
 
 <?
